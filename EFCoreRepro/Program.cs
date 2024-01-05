df0a4a84045d77063b3d0f1e73a7e5a9f14a196a
Hu Yao <yahu2@microsoft.com>
2024-01-05 20:50:29 +0800

    first commit

---
+++ EFCoreRepro/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using EFCoreRepro.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+DbContextOptions<MMPContext> options = new DbContextOptionsBuilder<MMPContext>()
+    .UseSqlServer(
+        "")
+    .UseQueryTrackingBehavior(QueryTrackingBehavior.TrackAll)
+    .UseLoggerFactory(LoggerFactory.Create(builder => { builder.AddConsole(); }))
+    .EnableSensitiveDataLogging()
+    .Options;
+MMPContext context = new MMPContext(options);
+
+Parent parentEntity = context.Parents.Add(new Parent(){ParentId = Guid.NewGuid(), Id = null}).Entity;
+context.Entry(parentEntity).State = EntityState.Added;
+await context.SaveChangesAsync(CancellationToken.None);
+
+Parent? parentEntity2 = await context.Parents.FindAsync(new object[] { parentEntity.ParentId }, CancellationToken.None);
+parentEntity2.Children.Add(new Child(){ChildId = Guid.NewGuid(), ParentId = parentEntity2.ParentId, Id = null});
+await context.SaveChangesAsync(CancellationToken.None);
